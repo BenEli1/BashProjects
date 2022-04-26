99e1ec5f310d1edfbc23924b4509e0e200b007dc
BenELi1 <ben.eli@live.biu.ac.il>
2022-04-26 20:25:17 +0300

    some changes

---
+++ filesTree.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function ftree {
+    SEDMAGIC='s;[^/]*/;|____;g;s;____|; |;g'
+
+    if [ "$#" -gt 0 ] ; then
+       dirlist="$@"
+    else
+       dirlist="."
+    fi
+
+    for x in $dirlist; do
+         find "$x" -print | sed -e "$SEDMAGIC"
+    done
+
+}
+
+ftree
--- gccfind.sh
+++ gccfind.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 #Ben Eli 319086435
+var="-r"
+if [ "$#" -lt "2" ]; then
+    echo "Not enough parameters"
+    exit 0
+fi
 
-for i in $(ls)
+
+for file in $(find . -name "*.out")
 do
+rm "$file"
+done
+
+
+for file in $(find . -name "*.c")
+do
+if grep -q "$2" "$file"; then
+  string=${file%".c"};
+gcc -w $file -o $string.out
+fi
+done
+
+if [[ "$3" == "$var" ]];
+then
+for f in $1; do
+    if [ -d "$f" ]; 
+    then
+    $($0 $f $2)
+    fi
+done
+fi
